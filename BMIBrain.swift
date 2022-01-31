8f3f7d9a27549e2fe2bdfc8f661b26895c17a76a
pedrohcalado <pedrocalado22@gmail.com>
2022-01-31 06:14:17 -0300

    All courses

---
+++ BMIBrain.swift
@@ -0,0 +1,9 @@
+//
+//  BMIBrain.swift
+//  BMI Calculator
+//
+//  Created by Pedro Henrique Calado on 20/10/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Pedro Henrique Calado on 22/10/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
